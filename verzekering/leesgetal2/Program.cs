8423328b751270b01e1e02b58486cb16ddf8c9a6
funzo88 <renzo-w@live.nl>
2017-05-19 09:36:30 +0200

    Add files via upload

---
+++ verzekering/leesgetal2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leesgetal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vul een getal in");
+            int getal = LeesGetal();
+            Console.WriteLine("Het getal wat je hebt ingevuld is {0}", getal);
+        }
+        private static int LeesGetal()
+        {
+            int getal = 0;
+            bool goed = false;
+            while(!goed)
+            {
+                string invoer = Console.ReadLine();
+                try
+                {
+                    getal = int.Parse(invoer);
+                    goed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Geen getal ingevoerd");
+                    goed = false;
+                }
+            }
+            return getal;
+        }
+    }
+}
---
+++ verzekering/verzekering/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verzekering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vul uw geboorte datum in op de volgende manier: dd/mm/yyyy");
+            int date = LeesDatum();
+        }
+        private static int LeesDatum()
+        {
+            int date = 01/01/2000;
+            bool goed = false;
+            while (!goed)
+            {
+                string invoer = Console.ReadLine();
+                try
+                {
+                    DateTime dt = DateTime.Parse(invoer);
+                    goed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Geen getal ingevoerd");
+                    goed = false;
+                }
+            }
+            return date;
+        }
+        private static int LeesGetal()
+        {
+            int getal = 0;
+            bool goed = false;
+            while (!goed)
+            {
+                string invoer = Console.ReadLine();
+                try
+                {
+                    getal = int.Parse(invoer);
+                    goed = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Geen getal ingevoerd");
+                    goed = false;
+                }
+            }
+            return getal;
+        }
+    }
+}
