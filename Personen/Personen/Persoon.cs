cdea4b5d3cd8c36e030a21b529b332a7bffce42a
funzo88 <renzo-w@live.nl>
2017-05-19 09:34:49 +0200

    Add files via upload

---
+++ Personen/Personen/Persoon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personen
+{
+    public class Persoon
+    {
+        private double _gewicht;
+        public double Gewicht { get { return _gewicht; } set { if (value > 0) { _gewicht = value; } } }
+        public Persoon(double gewicht)
+        {
+            Gewicht = gewicht;
+        }
+    }
+}
---
+++ Personen/Personen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persoon Henk = new Persoon(80);
+        }
+    }
+}
